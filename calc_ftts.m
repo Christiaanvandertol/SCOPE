bcfa71647c429728033a6a2d6c6313781a57e7be
Christiaan van der Tol <c.vandertol@utwente.nl>
2015-09-03 16:53:39 +0200

    remove unnessesary files

--- calc_ftts.m
+++
@@ -1,3 +0,0 @@
-function ftts = calc_ftts(P,ts)
-ts = sin(ts/180*pi);
-ftts = P(1)*ts.^4 + P(2)*ts.^3 + P(3)*ts.^2 + P(4)*ts + P(5);
--- calcfVcmo.m
+++
@@ -1,2 +0,0 @@
-function fVcmo = calcfVcmo(P,x)
-fVcmo = P(1)+(P(2)*x.^P(6)).*(1./(P(4)+P(3)*x.^P(5)));
