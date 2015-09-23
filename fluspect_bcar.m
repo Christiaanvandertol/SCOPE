e554e1f0693381253a1223bc671cec74ff0052f1
christiaan van der tol <c.vandertol@utwente.nl>
2015-09-23 17:53:30 +0200

    add license

--- fluspect_bcar.m
+++ fluspect_bcar.m
@@ -1,5 +1,21 @@
  function [leafopt] = fluspect_bcar(spectral,leafbio,optipar)
 %
+% Fluspect simulated reflectance, transmittance and fluorescence of leaves
+%     Copyright (C) 2015  Wout Verhoef and Christiaan van der Tol
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 % function [leafopt] = fluspect(spectral,leafbio,optipar)
 % calculates reflectance and transmittance spectra of a leaf using FLUSPECT, 
 % plus four excitation-fluorescence matrices
