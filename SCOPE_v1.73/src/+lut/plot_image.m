2431645dab02a58d9ee7137eccd561ad727f5c49
Prikaziuk <prikaziuk@gmail.com>
2020-02-05 11:41:23 +0100

    correct units

--- SCOPE_v1.73/src/+lut/plot_image.m
+++ SCOPE_v1.73/src/+lut/plot_image.m
@@ -2,7 +2,7 @@ function plot_image(im, what, out_path)
     figure
     imagesc(im)
     cb = colorbar;
-    title(cb, '[\mumol CO_2 cm^{-2} s^{-1}]')
+    title(cb, '[\mumol CO_2 m^{-2} s^{-1}]')
     title(['GPP with ' what])
     % if GPR is out of range (for SNAP), highly negative values appear
     caxis([quantile(im(:), 0.01), quantile(im(:), 0.99)])  
