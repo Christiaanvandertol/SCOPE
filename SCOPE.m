57fb6a5b4cea59905d3fa4e16b3fc3a69cf336be
Prikaziuk <prikaziuk@gmail.com>
2020-12-09 23:25:45 +0100

    first value from input data in ts

--- SCOPE.m
+++ SCOPE.m
@@ -107,7 +107,7 @@ clear('X')
 while ~feof(fid)
     line    = fgetl(fid);
     y       = textscan(line,'%s', 'Delimiter', ',', 'TreatAsEmpty',  ' ');
-    varnames(k)  = string(y{1}{1}); %#ok<SAGROW>
+    varnames(k)  = y{1}(1); %#ok<SAGROW>
     X(k).Val   = str2double(y{:});
     k       = k+1;
 end
@@ -185,6 +185,12 @@ end
 %% 11. load time series data
 if options.simulation == 1
     vi = ones(length(V),1);
+    for k = 1:length(V)
+        if ~strcmp(V(k).Name, 'Tparam') && length(V(k).Val) > 1
+            disp(V(k).Name)
+            V(k).Val = V(k).Val(1);
+        end
+    end
     [soil,leafbio,canopy,meteo,angles,xyt]  = select_input(V,vi,canopy,options,constants);
     [V, xyt, mly_ts, atmo_paths]  = load_timeseries(V, F, xyt, path_input);
 else
